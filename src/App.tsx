80d601183cab95b40316e306dcd486daeee3df41
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2025-05-27 21:37:53 -0300

    feat: added  useEffect hook to monitor the thema variable

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,18 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { Footer } from './components/Footer';
-
-import './styles/theme.css';
-import './styles/global.css';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
+import './styles/theme.css';
+import './styles/global.css';
 
 export function App() {
+
   return (
     <>
-      <Heading>Número: </Heading>
       <Container>
         <Logo />
       </Container>
