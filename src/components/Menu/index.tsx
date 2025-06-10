49038366f4fccad2575a17e2796f9280123d6b86
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2025-06-10 17:43:21 -0300

    fix: adjusting on index page

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,9 +4,6 @@ import styles from './styles.module.css';
 
 type AvailableThemes = 'dark' | 'light';
 
-//teste
-//teste
-
 export function Menu() {
   const [theme, setTheme] = useState<AvailableThemes>(() => {
     const storageTheme = localStorage.getItem('theme') as AvailableThemes || 'dark';
