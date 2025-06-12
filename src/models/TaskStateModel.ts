04dae660c7d0b17567eb75314bf7c72bfc506555
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2025-06-12 16:21:19 -0300

    note: notation on TaskStateModel

--- src/models/TaskStateModel.ts
+++ src/models/TaskStateModel.ts
@@ -1,8 +1,8 @@
 import { TaskModel } from "./TaskModel";
 
 export type TaskStateModel = {
-    tasks: TaskModel[];
-    secondsRemaining: number;
+    tasks: TaskModel[]; // histórico, MainForm
+    secondsRemaining: number; // Home, CountDown, Histórico
     formattedSecondsRemaining: string;
     activeTask: TaskModel | null;
     currentCycle: number; // 1 a 8
